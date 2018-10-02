08680a9aec525927ac377eaf5884c573e19956e9
Andrea Arighi <andrea@chino.io>
2018-10-02 17:43:17 +0200

    minor text fix

--- src/main/java/io/chino/java/ChinoBaseAPI.java
+++ src/main/java/io/chino/java/ChinoBaseAPI.java
@@ -237,12 +237,11 @@ public class ChinoBaseAPI {
      * @param offset the offset value in the request
      * @param length the length value in the request
      *
-     * @return JsonNode Object with the response of the server if there are no errors
+     * @return JsonNode Object with the response of the server (if there are no errors)
      *
      * @throws IOException data processing error
      * @throws ChinoApiException server error
      */
-    // TODO fix javadoc formatting in source code
     // TODO add all updatePartial methods to classes in io.chino.java
     public JsonNode putResource(String path, byte[] resource, int offset, int length) throws IOException, ChinoApiException {
         RequestBody requestBody = RequestBody.create(OCTET_STREAM, resource);
