acaee3f65ee28f37f11264e0dc1d5fdddd39dc8e
Andrea Arighi <andrea@chino.io>
2019-02-25 18:05:01 +0100

    Fix ResetAccount.java
    - Introduced flag --yes to skip input prompt
    - Added print of semi-obfuscated account information

--- src/test/java/io/chino/java/testutils/ResetAccount.java
+++ src/test/java/io/chino/java/testutils/ResetAccount.java
@@ -4,6 +4,7 @@ import io.chino.api.common.ChinoApiException;
 import io.chino.java.ChinoAPI;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ResetAccount {
@@ -14,10 +15,21 @@ public class ResetAccount {
     public static void main(String[] s) throws IOException, ChinoApiException {
         TestConstants.init(USERNAME, PASSWORD);
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Are you sure you want to delete EVERYTHING on your account?");
-        System.out.print("This operation cannot be undone. [yes/NO]> ");
-        String response = in.nextLine().toLowerCase();
+        String response;
+
+        if (Arrays.asList(s).contains("--yes")) {
+            response = "yes";
+        } else {
+            Scanner in = new Scanner(System.in);
+            System.out.println("--- YOUR ACCOUNT ----------------------" + "\n" +
+                               " ~ Chino.io host: " + TestConstants.HOST + "\n" +
+                               " ~ Customer ID: " + TestConstants.CUSTOMER_ID.substring(0, 5) + "...\n" +
+                               " ~ Customer key: ..." + TestConstants.CUSTOMER_KEY.substring(TestConstants.CUSTOMER_KEY.length() - 5) + "\n" +
+                               "---------------------------------------");
+            System.out.println("Are you sure you want to delete EVERYTHING on your account?");
+            System.out.print("This operation cannot be undone. [yes/NO]> ");
+            response = in.nextLine().toLowerCase();
+        }
 
         if (response.equals("yes")) {
             ChinoAPI chino_admin = new ChinoAPI(TestConstants.HOST, TestConstants.CUSTOMER_ID, TestConstants.CUSTOMER_KEY);
